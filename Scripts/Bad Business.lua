ba18359e3b26383e7ab0a929cff7d7ed09658282
xshonda <85666393+xshonda@users.noreply.github.com>
2021-06-15 23:34:41 -0500

    new script
    u cant scope with aimbot lel

--- Scripts/Bad Business.lua
+++ Scripts/Bad Business.lua
@@ -1,97 +1,273 @@
---// Setup & services.
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local UserInputService = game:GetService("UserInputService")
-
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-if not shared.SilentAim then --// If the table doesn't exist. Create one.
-    shared.SilentAim = {
-        Enabled = true,
-        HeadshotChance = 100
-    }
-end
-
---// Main script.
---// Of course you could just do require(game:GetService("ReplicatedStorage"):WaitForChild("TS")), but why do it the easy way?
-
-local TS = nil
-for Index, Value in pairs(getgc(true)) do --// Looping through the garbage collection.
-    if typeof(Value) == "table" and rawget(Value, "InitProjectile") then --// Seeing if we can find the Projectiles table by searching for the "InitProjectile" function that is paired to said table.
-        if Value.TS then --// If it has the game's main module in it then we successfully found the game's main module.
-            TS = Value.TS
-            break
+math.randomseed(tick());
+local modules, controlfunc;
+for i,v in next, getgc(true) do
+    if type(v) == "table" then
+        if rawget(v, "Version") and rawget(v, "Kitty") then
+            local mt = getrawmetatable(v);
+            modules = select(2, pcall(getupvalue, mt.__index, 1)); --gets the ts module (haha totally not stolen from kiriot :p)
+        elseif rawget(v, "Control") and type(rawget(v, "Control")) == "function" then
+            controlfunc = rawget(v, "Control"); --control function used for walkspeed mult
         end
     end
+    if modules and controlfunc then break; end
 end
-
-local function OnClientTeam(Player) --// Checking if the player we are looking for is on the client's (your) team using in-game functions.
-    if TS.Teams:ArePlayersFriendly(Player, LocalPlayer) then
-        return true
-    end
-
-    return false
+if not modules or (modules and not rawget(modules, "Network")) then
+    print("Failed To Get TS Module"); -- :(
+    return;
 end
+if not controlfunc then
+    print("Failed To Get Control Function"); -- :(
+    return;
+end
+local toggles = {
+    aimbot = false,
+    silentaim = false,
+    teamcheck = false,
+    vischeck = false,
+    bodypart = "Head",
+    hitchance = 1,
+    fov = 0,
+    norecoil = false,
+    nospread = false,
+    alwaysauto = false,
+    fastfirerate = false,
+    fly = false,
+    flyspeed = 100,
+};
+local oldmult = 1.8;
+local ok = false;
+local chartable = getupvalue(modules.Characters.GetCharacter, 1);
+local players = game:GetService("Players");
+local runservice = game:GetService("RunService");
+local uis = game:GetService("UserInputService");
+local camera = workspace.CurrentCamera;
+local localplayer = players.LocalPlayer;
+local mouse = localplayer:GetMouse();
+local isholding = false;
+local chamsholder = Instance.new("Folder", game:GetService("CoreGui"));
 
-local function GetHeadPart(Player) --// Getting the head part of a player.
-    local Character = TS.Characters:GetCharacter(Player)
-    local Body = (Character and Character:FindFirstChild("Body"))
 
-    if Character and Body then
-        return Body:FindFirstChild("Head")
-    end
-end 
 
-local function GetClosestPlayerCharacterToCursor()
-    local ClosestPlayer = nil
-    local FarthestDistance = math.huge
 
-    for Index, Player in pairs(Players:GetPlayers()) do --// Loop through all the players.
-        if Player == LocalPlayer then continue end --// Skip over this iteration if it is the Clients.
-        if OnClientTeam(Player) then continue end --// Skip over this iteration if the player is on the client's team.
-
-        local HeadPart = GetHeadPart(Player)
-
-        if HeadPart then
-            local HeadPosition, OnScreen = Workspace.CurrentCamera:WorldToViewportPoint(HeadPart.Position) --// Converting the 3D position to a screen position to make it easier.
-
-            if OnScreen then --// Check if they are visible/on screen in the roblox window area.
-                local MouseDistanceFromPlayer = (Vector2.new(HeadPosition.X, HeadPosition.Y) - UserInputService:GetMouseLocation()).Magnitude --// Check the mouse distance from the player.
-                
-                if MouseDistanceFromPlayer < FarthestDistance then
-                    FarthestDistance = MouseDistanceFromPlayer --// Make the farthest distance the mouse distance.
-                    ClosestPlayer = Player --// Closest player is the player iteration we are on for now, may change later on.
+local function getclosesttocursor()
+    local max, close = math.huge;
+    for player, character in next, chartable do
+        if player ~= localplayer and (toggles.teamcheck and not modules.Teams:ArePlayersFriendly(player, localplayer) or not toggles.teamcheck) and chartable[localplayer] then
+            local chosenpart = (character and character:FindFirstChild("Hitbox") and character.Hitbox:FindFirstChild(toggles.bodypart));
+            if chosenpart then --if part exist
+                local pos, onscreen = camera:WorldToScreenPoint(chosenpart.Position);
+                if onscreen then --if on screen (not to be confused with behind walls or not)
+                    local dist = Vector2.new(pos.X - mouse.X, pos.Y - mouse.Y).Magnitude;
+                    if dist <= max and dist <= toggles.fov then
+                        local part = camera:GetPartsObscuringTarget({chosenpart.Position}, {workspace.Characters, workspace.NonGeometry});
+                        if (toggles.vischeck and #part == 0 or not toggles.vischeck) then --gay but works
+                            max = dist;
+                            close = player;
+                        end
+                    end
                 end
             end
         end
     end
-
-    return TS.Characters:GetCharacter(ClosestPlayer) --// Return the player's character.
+    return close;
 end
 
-local OldFireProjectile = TS.Projectiles.InitProjectile --// Making a backup of the old function so we can use it to spoof later on.
-TS.Projectiles.InitProjectile = function(self, ...) --// I can't explain self well, but in this situation it is just the Projectiles table. Hooking the function.
-    local Arguments = {...} --// Converting the function arguments to a table.
-    
-    if shared.SilentAim.Enabled and Arguments[4] == LocalPlayer then
-        local ClosestPlayer = GetClosestPlayerCharacterToCursor()
-        local HeadshotChance = math.random(shared.SilentAim.HeadshotChance, 100)
-
-        --// Reference: https://coasts.cool/uploads/zo0BmqjIm0TBlmsy1Mkn.png
-        --// Position: Arguments[2]
-        --// Velocity: Arguments[3]
-
-        if ClosestPlayer and (ClosestPlayer.Health.Value > 0) then --// Check if it returned a player and the player is alive.
-            if HeadshotChance == 100 then --// If the random number hits 100 we goto the head because it got the top number.
-                Arguments[2] = (ClosestPlayer.Hitbox.Head.Position - Arguments[3])
-            else
-                Arguments[2] = (ClosestPlayer.Hitbox.Chest.Position - Arguments[3]) --// If not, we will hit at their chest.
+do-- hooks
+    local oldinit = modules.Projectiles.InitProjectile; --thanks coasts for method
+    local oldfire = modules.Network.Fire;
+    local oldlookvector = modules.Input.Reticle.LookVector;
+    local oldwait = modules.Timer.Wait;
+    local stopchanging = false; --ugly
+    local originalfuncs = {}; -- PLEASE DONT BULLY ME FOR THIS
+    modules.Projectiles.InitProjectile = function(self, ...)
+        local args = {...};
+        if args[4] == localplayer and toggles.silentaim and math.random(1, 100) <= toggles.hitchance then
+            local char = chartable[getclosesttocursor()];
+            local chosenpart = (char and char:FindFirstChild("Hitbox") and char.Hitbox:FindFirstChild(toggles.bodypart));
+            if chosenpart then
+                args[2] = chosenpart.Position - args[3];
+                return oldinit(self, unpack(args));
             end
-
-            --// Subtracting with the velocity to get the exact position to hit on the player's head.
-            --// From google: Velocity is the rate at which the position changes. The average velocity is the displacement or position change (a vector quantity) per time ratio.
+        end
+        return oldinit(self, ...);
+    end
+    modules.Network.Fire = function(self, ...)
+        local args = {...};
+        if args[1] == "Item_Paintball" and args[2] == "Shoot" then
+            if not stopchanging then
+                originalfuncs[8], originalfuncs[9], originalfuncs[10] = getupvalue(2, 8), getupvalue(2, 9), getupvalue(2, 10);
+                stopchanging = true;
+            end
+            setupvalue(2, 8, function(...) if toggles.norecoil then return; end return originalfuncs[8](...); end);
+            setupvalue(2, 9, function(...) if toggles.norecoil then return; end return originalfuncs[9](...); end);
+            setupvalue(2, 10, function(...) if toggles.norecoil then return; end return originalfuncs[10](...); end);
+            -- PLEASE FORGIVE ME I ONLY DID IT BECAUSE HOOKFUNCTION GAY :pleading_face:
+        end
+        return oldfire(self, ...);
+    end
+    modules.Input.Reticle.LookVector = function(...)
+        return (toggles.nospread and camera.CFrame.LookVector or oldlookvector(...));
+    end
+    modules.Timer.Wait = function(self, time)
+        local Upvals = getupvalues(2);
+        if toggles.fastfirerate then
+            if type(Upvals[1]) == "table" and rawget(Upvals[1], "Equipped") and typeof(Upvals[7]) == "Instance" and Upvals[7]:IsA("IntValue") then
+                return oldwait(self, 0);
+            end
+        end
+        return oldwait(self, time);
+    end
+end
+uis.InputBegan:Connect(function(key, gp)
+    ok = gp;
+    if not gp and key.UserInputType.Name == "MouseButton2" then
+        isholding = true;
+    end
+end)
+uis.InputEnded:Connect(function(key, gp)
+    if not gp and key.UserInputType.Name == "MouseButton2" then
+        isholding = false;
+    end
+end)
+runservice.RenderStepped:Connect(function()
+    math.randomseed(tick());
+    if toggles.aimbot then
+        if isholding and math.random(1, 100) <= toggles.hitchance then
+            local char = chartable[getclosesttocursor()];
+            local chosenpart = (char and char:FindFirstChild("Hitbox") and char.Hitbox:FindFirstChild(toggles.bodypart));
+            if chosenpart then
+                local pos, onscreen = camera:WorldToScreenPoint(chosenpart.Position);
+                if onscreen then
+                    mousemoverel(pos.X - mouse.X, pos.Y - mouse.Y);
+                end
+            end
         end
     end
-    
-    return OldFireProjectile(self, unpack(Arguments)) --// Unpacking the arguments. Basically replcaing parts of the game's fired arguments with our spoofed ones.
-end
+end)
+runservice.Stepped:Connect(function()
+    local flyvector = Vector3.new();
+    if uis:IsKeyDown("W") then
+        flyvector = flyvector + camera.CFrame.LookVector;
+    elseif uis:IsKeyDown("S") then
+        flyvector = flyvector - camera.CFrame.LookVector;
+    elseif uis:IsKeyDown("A") then
+        flyvector = flyvector - camera.CFrame.RightVector;
+    elseif uis:IsKeyDown("D") then
+        flyvector = flyvector + camera.CFrame.RightVector;
+    elseif uis:IsKeyDown("Space") then
+        flyvector = flyvector + camera.CFrame.UpVector;
+    elseif uis:IsKeyDown("LeftControl") then
+        flyvector = flyvector - camera.CFrame.UpVector;
+    end
+
+    if toggles.fly and chartable[localplayer] and not ok then
+        if flyvector.Unit.X == flyvector.Unit.X then
+            chartable[localplayer]:WaitForChild("Root").Velocity = flyvector.Unit * toggles.flyspeed;
+        end
+        --chartable[localplayer]:WaitForChild("Root").Anchored = toggles.fly and flyvector == Vector3.new();
+    end
+end)
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/edJT9EGX"))();
+
+local aimbot = library:CreateWindow("Aimbot");
+local weapons = library:CreateWindow("Weapons");
+local movement = library:CreateWindow("Movement");
+
+aimbot:AddToggle({
+    text = "Aimbot",
+    state = false,
+    callback = function(bool)
+        toggles.aimbot = bool;
+    end
+});
+aimbot:AddToggle({
+    text = "Silent Aim",
+    state = false,
+    callback = function(bool)
+        toggles.silentaim = bool;
+    end
+});
+aimbot:AddList({
+    text = "Aim Bone",
+    values = {"Head", "Chest"},
+    open = false,
+    callback = function(part)
+        toggles.bodypart = part;
+    end
+});
+aimbot:AddSlider({
+    text = "Hit Chance",
+    min = 1,
+    max = 100,
+    callback = function(hc)
+        toggles.hitchance = hc;
+    end
+});
+aimbot:AddSlider({
+    text = "FOV",
+    min = 0,
+    max = 800,
+    callback = function(fov)
+        toggles.fov = fov;
+    end
+});
+aimbot:AddToggle({
+    text = "Team Check",
+    state = false,
+    callback = function(bool)
+        toggles.teamcheck = bool;
+    end
+});
+aimbot:AddToggle({
+    text = "Visible Check",
+    state = false,
+    callback = function(bool)
+        toggles.vischeck = bool;
+    end
+});
+weapons:AddToggle({
+    text = "No Recoil",
+    state = false,
+    callback = function(bool)
+        toggles.norecoil = bool;
+    end
+});
+weapons:AddToggle({
+    text = "No Spread",
+    state = false,
+    callback = function(bool)
+        toggles.nospread = bool;
+    end
+});
+weapons:AddToggle({
+    text = "Fast Firerate",
+    state = false,
+    callback = function(bool)
+        toggles.fastfirerate = bool;
+    end
+});
+movement:AddSlider({
+    text = "Sprint Multiplier",
+    min = 0,
+    max = 10,
+    callback = function(value)
+        setconstant(controlfunc, table.find(getconstants(controlfunc), oldmult), value);
+        oldmult = value;
+    end
+});
+movement:AddToggle({
+    text = "Fly",
+    state = false,
+    callback = function(bool)
+        toggles.fly = bool;
+    end
+});
+movement:AddSlider({
+    text = "Fly Speed",
+    min = 100,
+    max = 200,
+    callback = function(speed)
+        toggles.flyspeed = speed;
+    end
+});
+library:Init();
