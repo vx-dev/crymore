f9e27b97de31d35f1cbf6c18c88a17b2d3b54178
xshonda <85666393+xshonda@users.noreply.github.com>
2021-06-15 23:33:08 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,9 +2,7 @@
 -- + Haze Infinity Loader by yuhsecurity +
 -- + Scripts Not Coded by Me             +
 -- +++++++++++++++++++++++++++++++++++++++
--- If Unnamed ESP isn't working enable show team
--- Unnamed ESP will be used to maintain functionality of games when there are no other scripts avalible
---
+-- If game is not supported by haze infinity a attempt will be made to execute Noclip & Infinite Jump
 
 
 print ('Join the discord - https://discord.gg/WJusFzwB29')
@@ -33,14 +31,6 @@ elseif game.PlaceId == 286090429 then -- Arsenal
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/Scripts/Arsenal.lua"))()
 --
-elseif game.PlaceId == 394206297 then -- Kinetic Code
-    local h = Instance.new("Message")
-    h.Parent = Workspace 
-    h.Text = "Haze Infinity | Kinetic Code Detected, Loading Script..."
-    wait(3)
-    h:Remove()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/Scripts/Unnamed%20ESP.lua"))()
---
 elseif game.PlaceId == 301549746 then -- Counter Blox
     local h = Instance.new("Message")
     h.Parent = Workspace 
@@ -48,20 +38,6 @@ elseif game.PlaceId == 301549746 then -- Counter Blox
     wait(3)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/Scripts/Counter%20Blox.lua"))()
---
-elseif game.PlaceId == 488667523 then -- Those Who Remain
-    h.Parent = Workspace
-    h.Text = "Haze Infinity | Those Who Remain Detected, Loading Script"
-    wait(3)
-    h:Remove()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/Scripts/Those%20Who%20Remain.lua"))()
-    --
-elseif game.PlaceId == 901793731 then -- D-DAY
-    h.Parent = Workspace
-    h.Text = "Haze Infinity | D-DAY Detected, Loading Script"
-    wait(3)
-    h:Remove()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/Scripts/D-DAY.lua"))()
 --
 else
 game.Players.LocalPlayer:Kick("Game Not Supported by Haze Infinity | discord.gg/WJusFzwB29") -- Trigger GNS Error
