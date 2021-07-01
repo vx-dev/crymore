04e52232f026decb24683d4caabc42708df6ab97
vx-dev <86169807+vx-dev@users.noreply.github.com>
2021-07-01 11:47:45 -0500

    rebranded

--- loader.lua
+++ loader.lua
@@ -1,22 +1,21 @@
---____   ____     ______      ___       ____       ____         _     ________  __________ ________    
---`MM'   `MM'     `M`MM(      )M'       `MM'      6MMMMb       dM.    `MMMMMMMb.`MMMMMMMMM `MMMMMMMb.  
--- MM     MM       M `MM.     d'         MM      8P    Y8     ,MMb     MM    `Mb MM      \  MM    `Mb  
--- MM     MM       M  `MM.   d'          MM     6M      Mb    d'YM.    MM     MM MM         MM     MM  
--- MM     MM       M   `MM. d'           MM     MM      MM   ,P `Mb    MM     MM MM    ,    MM     MM  
--- MM     MM       M    `MMd             MM     MM      MM   d'  YM.   MM     MM MMMMMMM    MM    .M9  
--- MM     MM       M     dMM.            MM     MM      MM  ,P   `Mb   MM     MM MM    `    MMMMMMM9'  
--- MM     MM       M    d'`MM.           MM     MM      MM  d'    YM.  MM     MM MM         MM  \M\    
--- MM     YM       M   d'  `MM.          MM     YM      M9 ,MMMMMMMMb  MM     MM MM         MM   \M\   
--- MM    / 8b     d8  d'    `MM.         MM    / 8b    d8  d'      YM. MM    .M9 MM      /  MM    \M\  
---_MMMMMMM  YMMMMM9 _M(_    _)MM_       _MMMMMMM  YMMMM9 _dM_     _dMM_MMMMMMM9'_MMMMMMMMM _MM_    \M\_
-
-
+--                                                                                                
+--  ,ad8888ba,                            88b           d88                                       
+-- d8"'    `"8b                           888b         d888                                       
+--d8'                                     88`8b       d8'88                                       
+--88             8b,dPPYba,  8b       d8  88 `8b     d8' 88   ,adPPYba,   8b,dPPYba,   ,adPPYba,  
+--88             88P'   "Y8  `8b     d8'  88  `8b   d8'  88  a8"     "8a  88P'   "Y8  a8P_____88  
+--Y8,            88           `8b   d8'   88   `8b d8'   88  8b       d8  88          8PP"""""""  
+-- Y8a.    .a8P  88            `8b,d8'    88    `888'    88  "8a,   ,a8"  88          "8b,   ,aa  
+--  `"Y8888Y"'   88              Y88'     88     `8'     88   `"YbbdP"'   88           `"Ybbd8"'  
+--                               d8'                                                              
+--                              d8'                                                               
+--
 print ('Join the discord - https://discord.gg/KTvtJdu7eG')
 --
 if game.PlaceId == 292439477 then -- Phantom Forces
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Phantom Forces Detected, Loading Script..."
+    h.Text = "CryMore Loader | Phantom Forces Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/Phantom%20Forces.lua"))()
@@ -25,7 +24,7 @@ if game.PlaceId == 292439477 then -- Phantom Forces
 elseif game.PlaceId == 3233893879 then -- Bad Business
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Bad Business Detected, Loading Script..."
+    h.Text = "CryMore Loader | Bad Business Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/Bad%20Business.lua"))()
@@ -33,7 +32,7 @@ elseif game.PlaceId == 3233893879 then -- Bad Business
 elseif game.PlaceId == 286090429 then -- Arsenal
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Arsenal Detected, Loading Script..."
+    h.Text = "CryMore Loader | Arsenal Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/Arsenal.lua"))()
@@ -41,7 +40,7 @@ elseif game.PlaceId == 286090429 then -- Arsenal
 elseif game.PlaceId == 301549746 then -- Counter Blox
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Counter Blox Detected, Loading Script..."
+    h.Text = "CryMore Loader | Counter Blox Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/hexagon/main/scripts/CounterBlox.lua"))()
@@ -49,7 +48,7 @@ elseif game.PlaceId == 301549746 then -- Counter Blox
 elseif game.PlaceId == "6673614635" then -- unConventional
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | unConventional Detected, Loading Script..."
+    h.Text = "CryMore Loader | unConventional Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/unConventional"))()
@@ -57,7 +56,7 @@ elseif game.PlaceId == "6673614635" then -- unConventional
 elseif game.PlaceId == "6872265039" then -- Bedwars
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Bedwars Detected, Loading Script..."
+    h.Text = "CryMore Loader | Bedwars Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/Bedwars"))()
@@ -65,10 +64,10 @@ elseif game.PlaceId == "6872265039" then -- Bedwars
 elseif game.PlaceId == "3527629287" then -- Big Paintball
     local h = Instance.new("Message")
     h.Parent = Workspace 
-    h.Text = "Lux Loader | Big Paintball Detected, Loading Script..."
+    h.Text = "CryMore Loader | Big Paintball Detected, Loading Script..."
     wait(10)
     h:Remove()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/lux/main/Scripts/Big%20Paintball"))()
 else
-game.Players.LocalPlayer:Kick("Game Not Supported by Lux Loader | https://discord.gg/KTvtJdu7eG") -- Trigger GNS Error
+game.Players.LocalPlayer:Kick("Game Not Supported by CryMore Loader | https://discord.gg/KTvtJdu7eG") -- Trigger GNS Error
 end
