b560bcc2074cba57b6a61d7a45b5021306458a7d
vx-dev <86169807+vx-dev@users.noreply.github.com>
2021-07-18 16:21:57 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -14,7 +14,6 @@ print ('Join the discord - https://discord.gg/KTvtJdu7eG')
 --
 if game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/crymore/main/Scripts/msg.lua"))()
-
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vx-dev/crymore/main/Scripts/Phantom%20Forces.lua"))()
 --
 elseif game.PlaceId == 3233893879 then -- Bad Business
